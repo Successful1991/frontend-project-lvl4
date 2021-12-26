f6abaf26a29afc0bda9e83cfaf2b9e405957cf94
Successful1991 <19successful91@gmail.com>
2021-12-26 14:43:55 +0200

    change gitignore

--- src/components/rollbar.jsx
+++ src/components/rollbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider, ErrorBoundary } from '@rollbar/react';
 import Rollbar from 'rollbar';
 
+const TOKEN_ACCESS_ROlLBAR = "18cb28795688419688d97d333e82211f";
 const rollbarConfig = {
   accessToken: TOKEN_ACCESS_ROlLBAR,
   captureUncaught: true,
