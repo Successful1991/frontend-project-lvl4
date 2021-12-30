a6e36877af89670c50375aca0d403768b8c940c2
Successful1991 <19successful91@gmail.com>
2021-12-30 18:43:14 +0200

    refactoring code

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,6 +1,6 @@
 import React, {useCallback, useContext, useEffect, useState} from 'react';
 import {Formik} from 'formik';
-import {Form} from 'react-bootstrap';
+import {Form, Button} from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { authContext, serviceContext } from '../contexts';
 import {useTranslation} from 'react-i18next';
@@ -80,14 +80,14 @@ const Messages = () => {
                 className='form__input'
               />
               <div className='input-group-append form__btn'>
-                <button onClick={handleSubmit} className='btn btn-group-vertical' disabled={!values.message} aria-label='Новое сообщение' >
+                <Button type="submit" className='btn btn-group-vertical' disabled={!values.message} aria-label='Новое сообщение' >
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                        className="bi bi-arrow-right-square" viewBox="0 0 16 16">
                     <path fillRule="evenodd"
                           d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
                   </svg>
                   <span className='visually-hidden'>Отправить</span>
-                </button>
+                </Button>
               </div>
             </Form>
           )}
