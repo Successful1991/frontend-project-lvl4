102220d2b42c0735c7273aafdddf29dc85e6525c
Successful1991 <19successful91@gmail.com>
2022-01-16 17:18:33 +0200

    refactoring code

--- src/components/rollbar.jsx
+++ src/components/rollbar.jsx
@@ -3,14 +3,14 @@ import { Provider, ErrorBoundary } from '@rollbar/react';
 import Rollbar from 'rollbar';
 
 const rollbarConfig = {
-  // accessToken: TOKEN_ACCESS_ROLLBAR, //так не работает
-  accessToken: '18cb28795688419688d97d333e82211f', // TOKEN_ACCESS_ROLLBAR,
+  accessToken: '18cb28795688419688d97d333e82211f',
   captureUncaught: true,
   captureUnhandledRejections: true,
   payload: {
     environment: 'production',
   },
 };
+
 const rollbarInstance = new Rollbar(rollbarConfig);
 
 export default function RollbarProvider({ children }) {
